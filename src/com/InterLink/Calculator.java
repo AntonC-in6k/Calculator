1f058695c3e2b1f4c6098d701be0b65d34141b09
employee <antonc@softwareplanet.uk.com>
2016-07-08 11:14:46 +0300

    he method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”

--- src/com/InterLink/Calculator.java
+++ src/com/InterLink/Calculator.java
@@ -7,7 +7,17 @@ import java.util.List;
  */
 public class Calculator {
     public static int add(String input){
+
+        if (input.length()==0){return 0;}
+        String[] digitsForAdding=input.split(",");
         int result=0;
+        if (digitsForAdding.length==1){result+=Integer.parseInt(digitsForAdding[0]);}
+        if (digitsForAdding.length==2){
+            result+=Integer.parseInt(digitsForAdding[0])+Integer.parseInt(digitsForAdding[1]);
+        }
+        if (digitsForAdding.length>2){
+            throw new RuntimeException("You must enter now more than 2 numbers");
+        }
         return result;
     }
 }
--- test/tddrequirementspractice/TDDPracticeTests.java
+++ test/tddrequirementspractice/TDDPracticeTests.java
@@ -6,6 +6,8 @@ import com.InterLink.Calculator;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by employee on 7/8/16.
  */
@@ -13,8 +15,25 @@ public class TDDPracticeTests {
 
 
     @Test
-    public void createSimpleCalculatorTest() throws Exception {
-        assertThat(Calculator.add());
-
+    public void inputEmptyStringTest() throws Exception {
+        assertThat(Calculator.add(""), is(0));
     }
+    @Test
+    public void inputOneNumberTest() throws Exception {
+        assertThat(Calculator.add("1"), is(1));
+    }
+    @Test
+    public void inputTwoNumbersTest() throws Exception{
+        assertThat(Calculator.add("1,2"), is(3));
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void inputMoreThanTwoNumbersTest() throws Exception{
+        Calculator.add("1,2,3");
+    }
+    @Test (expected = RuntimeException.class)
+    public void inputNotNumberTest() throws Exception{
+        Calculator.add("1,x");
+    }
+
 }
