5477cfe60ddc89fb0548b1304f699120c021afc6
employee <antonc@softwareplanet.uk.com>
2016-07-08 14:27:13 +0300

    delimiters can be of any length with the following format: “//[delimiter]\n” for example: “//[—]\n1—2—3” should return 6

--- src/com/InterLink/Calculator.java
+++ src/com/InterLink/Calculator.java
@@ -6,22 +6,28 @@ import java.util.List;
  * Created by employee on 7/8/16.
  */
 public class Calculator {
+
     public static int add(String input) {
 
         if (input.length() == 0) {
             return 0;
         }
-        String customDelimiter = "\n";
+        String customDelimiter;
+        String delimiters=",|\n";
         if (input.contains("//") == true) {
             customDelimiter = input;
             customDelimiter = customDelimiter.substring(0, customDelimiter.indexOf("\n")).substring(2);
-            input = input.substring(3 + customDelimiter.length());
-            System.out.print(input);
+            if (customDelimiter.startsWith("[")&&customDelimiter.endsWith("]")){
+                customDelimiter=customDelimiter.substring(0, customDelimiter.length()-1).substring(1);
+            }
+            input = input.substring(5 + customDelimiter.length());
+            delimiters+="|"+customDelimiter;
+            System.out.print(customDelimiter);
         }
 
         int counterOfNegativeDigits = 0;
         String groupOfNegativeDigits = "";
-        String[] digitsForAdding = input.split(",|\n|" + customDelimiter);
+        String[] digitsForAdding = input.split(delimiters);
 
         int result = 0;
         for (String digit :
