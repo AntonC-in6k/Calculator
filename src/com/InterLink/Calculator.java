94667e13ff64948c6bf1c0152c727322ceaf4712
employee <antonc@softwareplanet.uk.com>
2016-07-08 11:52:53 +0300

    Support different delimiters To change a delimiter, the beginning of the string will contain a separate line that looks like this: “//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’ . The first line is optional. All existing scenarios should still be supported

--- src/com/InterLink/Calculator.java
+++ src/com/InterLink/Calculator.java
@@ -6,11 +6,22 @@ import java.util.List;
  * Created by employee on 7/8/16.
  */
 public class Calculator {
-    public static int add(String input){
+    public static int add(String input) {
 
-        if (input.length()==0){return 0;}
-        String[] digitsForAdding=input.split(",|\n");
-        int result=0;
+        if (input.length() == 0) {
+            return 0;
+        }
+        String customDelimiter = "\n";
+        if (input.contains("//") == true) {
+            customDelimiter = input;
+            customDelimiter = customDelimiter.substring(0, customDelimiter.indexOf("\n")).substring(2);
+            input = input.substring(3 + customDelimiter.length());
+        }
+
+
+        String[] digitsForAdding = input.split(",|\n|" + customDelimiter);
+
+        int result = 0;
         for (String digit :
                 digitsForAdding) {
             result += Integer.parseInt(digit);
--- test/tddrequirementspractice/TDDPracticeTests.java
+++ test/tddrequirementspractice/TDDPracticeTests.java
@@ -17,29 +17,35 @@ public class TDDPracticeTests {
     public void inputEmptyStringTest() throws Exception {
         assertThat(Calculator.add(""), is(0));
     }
+
     @Test
     public void inputOneNumberTest() throws Exception {
         assertThat(Calculator.add("1"), is(1));
     }
+
     @Test
-    public void inputTwoNumbersTest() throws Exception{
+    public void inputTwoNumbersTest() throws Exception {
         assertThat(Calculator.add("1,2"), is(3));
     }
 
-    @Test (expected = RuntimeException.class)
-    public void inputNotNumberTest() throws Exception{
+    @Test(expected = RuntimeException.class)
+    public void inputNotNumberTest() throws Exception {
         Calculator.add("1,x");
     }
 
     @Test
-    public void inputManyNumbersTest() throws Exception{
+    public void inputManyNumbersTest() throws Exception {
         assertThat(Calculator.add("1,2,3,4,5"), is(15));
     }
 
     @Test
-    public void inputNumbersSeparatedByNewLineTest() throws Exception{
+    public void inputNumbersSeparatedByNewLineTest() throws Exception {
         assertThat(Calculator.add("1\n2,3,4,5"), is(15));
     }
 
+    @Test
+    public void inputCustomDelimitersTest() throws Exception {
+        assertThat(Calculator.add("//;\n1,2,3;4,5"), is(15));
+    }
 
 }
